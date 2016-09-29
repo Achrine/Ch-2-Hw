56c420240514f1115bdc2c36942dae8a243ae786
Steven Brcak <achrine1@gmail.com>
2016-09-29 15:16:38 -0700

    I know some of these shouldn't be here, but, fuck it.

---
+++ Ch2-10/hw_template.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  Ch2-6
+//
+//  Created by Steven Brcak on 9/29/16.
+//  Copyright © 2016 Steven Brcak. All rights reserved.
+//
+
+// Assignment: Program Challenge
+//             Chapter 2 number 99
+//Filename:    Ch...
+//Creation Date:
+//Description;
+
+
+//Preprocessor Directive
+//this line of code causes the contens of another file to be inserted into the program
+
+#include <iostream> 
+using namespace std;
+
+//the following line of code begins every C++ program
+
+int main(){
+    //variable definition statements
+    
+    //input
+    //Variable Assignment statement
+    
+    //Processing
+    //Variable Assignment Statement using an expression
+    
+    //output to screen
+    cout << "blah" << endl;
+    
+    //The next line of code reads: Return Zero
+    return 0;
+}
