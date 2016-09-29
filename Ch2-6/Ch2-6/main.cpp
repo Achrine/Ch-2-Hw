7cbf5872ab339e8b9d10de4f41ef318788e49a97
Steven Brcak <achrine1@gmail.com>
2016-09-29 15:25:22 -0700

    Everything is ruined in this repo

--- Ch2-6/Ch2-6/main.cpp
+++ Ch2-6/Ch2-6/main.cpp
@@ -11,7 +11,13 @@
 //Filename:    Ch...
 //Creation Date:
 //Description;
-
+/*
+ Suppose an employee gets paid every two weeks and earns $1700.00 each pay period. In a year the employee gets paid 26 times. Write a program thnaets dthee following variables:
+ payAmount This variable will hold the amount of pay the employee earns each pay period. Initialize the variable with 1700.0.
+ payPeriods This variable will hold the number of pay periods in a year . Initialize the variable with 26.
+ annualPay This variable will hold the employee s total annual pay , which will be calculated.
+ The program should calculate the employee s total annual pay by multiplying the employee s pay amount by the number of pay periods in a year , and store the result in the annualPay variable. Display the total annual pay on the screen.
+*/
 
 //Preprocessor Directive
 //this line of code causes the contens of another file to be inserted into the program
@@ -23,7 +29,9 @@ using namespace std;
 
 int main(){
     //variable definition statements
-    
+    double payAmount = 1700.00;
+    int payPeriod = 26;
+    double annualPay = payAmount * payPeriod;
     //input
     //Variable Assignment statement
     
@@ -31,7 +39,7 @@ int main(){
     //Variable Assignment Statement using an expression
     
     //output to screen
-    cout << "blah" << endl;
+    cout << "He makes: " << annualPay << " a year" << endl;
     
     //The next line of code reads: Return Zero
     return 0;
